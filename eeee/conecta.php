4344f95f7225e898b3d0b679a1cce0df9b28e144
dcs136 <dcs136@hotmail.com>
2021-01-11 19:12:19 -0300

    Test4

--- eeee/conecta.php
+++
@@ -1,9 +0,0 @@
-<?php
- $conexao = mysqli_connect('sql101.unaux.com', 'unaux_26935305', 's53ykvtj', 'unaux_26935305_tcc');
-
- mysqli_set_charset($conexao, 'utf8');
-
- if($conexao -> connect_error){
-  die("Falha ao realizar a conexao: ".$conexao -> connect_error);
- }
- ?>
