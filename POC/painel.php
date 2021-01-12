f92f5360537726263e2148d16aec53d0003a57ae
dcs136 <dcs136@hotmail.com>
2021-01-11 22:33:51 -0300

    Test4

--- POC/painel.php
+++ POC/painel.php
@@ -11,6 +11,15 @@ include('verifica_login.php');
 	<meta charset="utf-8">
 	<title>Comprar</title>
 	<link rel="stylesheet" type="text/css" href="mdss.css">
+<script type = "text/javascript">
+
+		function preventBack(){window.history.forward();}
+		
+		setTimeout("preventBack()");
+		
+		window.onunload=function(){null};
+
+</script>
 </head>
 <body>
 <a href="/construção/construção.html">
