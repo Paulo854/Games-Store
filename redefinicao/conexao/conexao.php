8b51c4152a4b2f9264b66e206bb580b5c21b860b
dcs136 <dcs136@hotmail.com>
2021-01-12 16:39:39 -0300

    Test4

--- redefinicao/conexao/conexao.php
+++ redefinicao/conexao/conexao.php
@@ -2,6 +2,6 @@
 define('HOST', '127.0.0.1:50871');
 define('USUARIO', 'azure');
 define('SENHA', '6#vWHD_$');
-define('DB', 'login');
+define('DB', 'gamestore');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
