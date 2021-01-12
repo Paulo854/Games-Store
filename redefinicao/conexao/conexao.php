f795146f8c4fdfffe55848eff189d49aaffd6b0b
dcs136 <dcs136@hotmail.com>
2021-01-12 17:40:08 -0300

    t

--- redefinicao/conexao/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST', '127.0.0.1:50871');
-define('USUARIO', 'azure');
-define('SENHA', '6#vWHD_$');
-define('DB', 'login');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
