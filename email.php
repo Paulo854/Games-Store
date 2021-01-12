87ed54480a857083fd15cf96706d92cdda2d1e4e
dcs136 <dcs136@hotmail.com>
2021-01-11 21:15:16 -0300

    Test4

--- email.php
+++ email.php
@@ -10,14 +10,12 @@ if(isset($_POST['sendemail'])){
 $email = new \SendGrid\Mail\Mail();
 $email->setFrom("pchefea44@gmail.com", "Example User");
 $email->addTo($email, $nome);
-$email->addContent("text/plain", $mensagem);
+$email->addContent("text/plain", $mensagem);	
 $sendgrid = new \SendGrid(getenv('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'));
 
-if(sendgrid->send($email));{
+if(sendgrid->send($email)){
 
 	echo "Email Enviado com Sucesso";
 
 }
-}
-
 ?>
