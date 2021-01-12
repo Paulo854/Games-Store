9a6fc307af4c0372bb2cb820d157fd183596a90b
dcs136 <dcs136@hotmail.com>
2021-01-11 23:06:38 -0300

    Test4

--- cadastro/index.php
+++ cadastro/index.php
@@ -1,88 +1,34 @@
 <?php
-    session_start();
-
-    if(isset($_SESSION["usuario"]) && is_array($_SESSION["usuario"])){
-        echo "<script>window.location = 'dashboard.php'</script>";
+  $login_cookie = $_COOKIE['login'];
+    if(isset($login_cookie)){
+      echo"Bem-Vindo, $login_cookie <br>";
+      echo"Essas informações <font color='red'>PODEM</font> ser acessadas por você";
+    }else{
+      echo"Bem-Vindo, convidado <br>";
+      echo"Essas informações <font color='red'>NÃO PODEM</font> ser acessadas por você";
     }
 ?>
+
+<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8" />
-        <title>Sistema de acesso</title>
-        <link rel="stylesheet" type="text/css" href="style/acesso.css" />
-        <script type="text/javascript" src="script/jquery.js"></script>
-        <script type="text/javascript" src="script/acesso.js"></script>
-    </head>
-    <body>
-        <header>Sistema de acesso</header>
+<head>
+	<meta charset="utf-8">
+	<title>ATEÇÂO!!!</title>
+	<style type="text/css">
+		body{
+			background-color: white;
+		}
+	</style>
+  <script language="JavaScript">
+    function protegercodigo() {
+    if (event.button==2||event.button==3){
+        alert('Codigo protegido!');}
+    }
+    document.onmousedown=protegercodigo
+</script>
+<meta http-equiv="refresh" content="0;http://gamesstore.unaux.com/"></meta>
+</head>
+<body>
 
-        <div id="subheader">
-            <ul>
-                <li><a href="javascript:void(0)">home</a></li>
-                <li><a href="javascript:void(0)">sobre</a></li>
-                <li><a href="javascript:void(0)">produtos</a></li>
-                <li><a href="javascript:void(0)">contato</a></li>
-            </ul>
-        </div>
-
-        <div id="mensagem"></div>
-
-        <div id="formulario">
-            <form id="formularioLogin">
-                <span class="title">Acesse sua conta</span>
-
-                <div id="linha">
-                    <label for="email">Email</label>
-                    <input type="text" name="email" id="email" />
-                </div>
-
-                <div id="linha" class="senha">
-                    <label for="senha">Senha</label>
-                    <input type="password" name="senha" id="senha" />
-                </div>
-
-                <div id="button">
-                    <button id="btnEntrar">Entrar</button>
-                    <a href="javascript:void(0)" id="esqueciSenha">Esqueci minha senha</a>
-                </div>
-            </form>
-
-            <form id="formularioCadastro">
-                <span class="title">Crie sua conta</span>
-
-                <div id="linha">
-                    <label for="nomeCadastro">Nome</label>
-                    <input type="text" name="nomeCadastro" id="nomeCadastro" />
-                </div>
-
-                <div id="linha">
-                    <label for="emailCadastro">Email</label>
-                    <input type="text" name="emailCadastro" id="emailCadastro" />
-                </div>
-
-                <div id="linha">
-                    <label for="senhaCadastro">Senha</label>
-                    <input type="password" name="senhaCadastro" id="senhaCadastro" />
-                </div>
-
-                <div id="button">
-                    <button id="btnCadastrar">Cadastrar</button>
-                </div>
-            </form>
-
-            <div id="textoCadastro">
-                <span class="title">Não possui uma conta?</span>
-                <span class="subtitle">Crie uma conta agora para acessar todas as ferramentas. É de graça!</span>
-                <button id="btnCadastro" class="change">Cadastrar</button>
-            </div>
-
-            <div id="textoLogin">
-                <span class="title">Já possui uma conta?</span>
-                <span class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam modi, et iusto explicabo amet libero saepe quos impedit quisquam ut, ex tempora.</span>
-                <button id="btnLogin" class="change">Entrar</button>
-            </div>
-        </div>
-
-        <footer>Sistema de acesso &copy; 2020</footer>
-    </body>
+</body>
 </html>
